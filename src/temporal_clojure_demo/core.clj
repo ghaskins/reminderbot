080d6d5976ed6236bf3ec0389d53aadc9f06d347
Greg Haskins <greg@manetu.com>
2023-01-13 18:23:39 -0500

    Skeleton project
    Signed-off-by: Greg Haskins <greg@manetu.com>

---
+++ src/temporal_clojure_demo/core.clj
@@ -0,0 +1,7 @@
+(ns temporal-clojure-demo.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/temporal_clojure_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns temporal-clojure-demo.core-test
+  (:require [clojure.test :refer :all]
+            [temporal-clojure-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
